baf2dbc39d01f609fa0afcb99fd5e939c841d0ba
diacrami <diacrami@espol.edu.ec>
2020-06-25 14:28:27 -0500

    Clase abstracta aderezo y subclases tipos de aderezos.

---
+++ src/Adicionales/Frutilla.java
@@ -0,0 +1,18 @@
+package Adicionales;
+
+public class Frutilla extends Aderezo {
+	
+        
+        
+	@Override
+	public String setNombre() {
+		// TODO Auto-generated method stub
+		return "Frutilla";
+	}
+      
+
+	
+	
+
+}
+
---
+++ src/Adicionales/Malvavisco.java
@@ -0,0 +1,16 @@
+package Adicionales;
+
+public class Malvavisco extends Aderezo{
+
+        
+       
+	@Override
+	public String setNombre() {
+		return "Malvavisco";
+		
+	}
+        
+
+
+	
+}
